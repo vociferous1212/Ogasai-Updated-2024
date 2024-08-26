ece6ab2a22a7cd780dd63afe08477418754b6714
vociferous1212 <garrettp@duck.com>
2024-08-26 14:15:49 -0400

    bot wasn't moving back to hotspot after vendoring

--- scripts/nav/script_moveToHotspot.lua
+++ scripts/nav/script_moveToHotspot.lua
@@ -7,15 +7,13 @@ function script_moveToHotspot:moveToHotspot(localObj)
 	if (script_getSpells.getSpellsStatus == 1) then
 		return false;
 	end
-	--if (IsInCombat()) or (script_grindEX:checkForTargetsOnHotspotRoute() and script_grind.attackTargetsOnRoutes) then
-	--	return false;
-	--end
+
 	if (script_nav.currentHotSpotName ~= 0) and (not script_checkDebuffs:hasDisabledMovement()) and (script_nav.numSavedLocation < 3) then
 		if (not script_grind.adjustTickRate) then
 			script_grind.tickRate = 135;
 		end
 		
--- move to hotspot coords
+		-- move to hotspot coords
 		if (not script_grind.hotspotReached) then
 
 			if (not IsInCombat()) and (not script_gather.gathering) then
@@ -24,38 +22,46 @@ function script_moveToHotspot:moveToHotspot(localObj)
 			
 		end
 			-- mount/stealth/cat form/ travel form/ ghost wolf
-			if (not IsMounted() and not script_grind.useMount) and (HasSpell("Stealth") or HasSpell("Cat Form") or HasSpell("Travel Form") or HasSpell("Ghost Wolf")) and (not IsIndoors()) then
-				if (not script_checkDebuffs:hasPoison()) and (script_rogue.useStealth or script_druid.useStealth) and (not IsSpellOnCD("Stealth")) and (not IsSpellOnCD("Prowl")) then
-					CastStealth();
-				end
-			--	CastGhostWolf();
-				if (HasSpell("Sprint")) and (not IsSpellOnCD("Sprint")) then
-					CastSprint();
-				end
+		if (not IsMounted() and not script_grind.useMount) and
+			(HasSpell("Stealth") or HasSpell("Cat Form") or HasSpell("Travel Form") or HasSpell("Ghost Wolf")) and (not IsIndoors()) then
+			if (not script_checkDebuffs:hasPoison()) and (script_rogue.useStealth or script_druid.useStealth) and
+				(not IsSpellOnCD("Stealth")) and (not IsSpellOnCD("Prowl")) then
+				CastStealth();
 			end
+			--CastGhostWolf();
+			if (HasSpell("Sprint")) and (not IsSpellOnCD("Sprint")) then
+				CastSprint();
+			end
+		end
 		
-	-- stop moving so we can mount
-			if (not IsInCombat()) and (not IsMounted()) and (not IsIndoors()) and (not HasForm()) and (script_grind.useMount) then
-				if (IsMoving()) then
-					StopMoving();
-					return;
-				end
-			return;
+		-- stop moving so we can mount
+		if (not IsInCombat()) and (not IsMounted()) and (not IsIndoors()) and (not HasForm()) and (script_grind.useMount) then
+			if (IsMoving()) then
+				StopMoving();
+				return;
 			end
+		return;
+		end
 	
-	-- display distance to hotspot in message window
-			local hsDist = 0;
-			if (script_nav:getDistanceToHotspot() ~= nil and script_nav:getDistanceToHotspot() ~= 0) then
-				hsDist = math.floor(script_nav:getDistanceToHotspot());
-			end
+		-- display distance to hotspot in message window
+		local hsDist = 0;
+		if (script_nav:getDistanceToHotspot() ~= nil and script_nav:getDistanceToHotspot() ~= 0) then
+			hsDist = math.floor(script_nav:getDistanceToHotspot());
+		end
 	
 		if (not script_grind.hotspotReached) and (script_getSpells.getSpellsStatus < 1) then
 			self.message = script_navEX:moveToTarget(localObj, script_nav.currentHotSpotX, script_nav.currentHotSpotY, script_nav.currentHotSpotZ);
 		end
 
-		return "Moving to hotspot " .. script_nav.currentHotSpotName .. " Dist (yds) " ..hsDist.. "";
+		
 	else
-		return "No hotspot has been loaded...";
+		script_navEX:moveToTarget(localObj, script_nav.currentHotSpotX, script_nav.currentHotSpotY, script_nav.currentHotSpotZ);
+		return "Moving to hotspot " .. script_nav.currentHotSpotName .. " Dist (yds) " ..hsDist.. "";
 	end
+	--elseif (script_nav.numSavedLocation >= 3) then
+	--	self.message = script_nav:moveToSavedLocation(localObj, self.minLevel, self.maxLevel, self.staticHotSpot);
+	--else
+	--	return "No hotspot has been loaded...";
+	--end
 return false;
 end
