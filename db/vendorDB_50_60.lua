61a16fd092879569303f449d8750555ad222a2cf
vociferous1212 <garrettp@duck.com>
2024-08-31 12:45:15 -0400

    Delete db directory

--- db/vendorDB_50_60.lua
+++
@@ -1,8 +0,0 @@
-vendorDB_50_60 = {}
-
-function vendorDB_50_60:setup()
-
-	--- (name, faction, continentID, mapID, canRepair, hasFood, hasWater, hasArrow, hasBullet, posX, posY, posZ, race)
-
-
-end
